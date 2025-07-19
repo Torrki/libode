3998fce52a16085e5d93bf628ec1c455d368afff
Torrki <kioer.exe@gmail.com>
2025-07-19 20:40:40 +0200

    Aggiunto LMM
    Signed-off-by: Torrki <kioer.exe@gmail.com>

--- include/ode.h
+++ include/ode.h
@@ -6,3 +6,4 @@ gsl_matrix* EuleroIndietro(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double 
 gsl_matrix* CrankNicolson(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double T, double h, const gsl_vector* y0);
 gsl_matrix* Heun(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double T, double h, const gsl_vector* y0);
 gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double T, double h, const gsl_vector* y0, gsl_matrix* A, gsl_vector* B);
+gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double T, double h, gsl_matrix* innesco, gsl_vector* A, gsl_vector* B, double b_1);
--- src/ode.c
+++ src/ode.c
@@ -203,3 +203,83 @@ gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),do
   gsl_vector_free(f_k);
   return O_sim;
 }
+
+gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double T, double h, gsl_matrix* innesco, gsl_vector* A, gsl_vector* B, double b_1){
+  const unsigned N_h=(unsigned)(floor(T/h)+1.0);
+  
+  //Allocazione matrice
+  gsl_matrix* O_sim=gsl_matrix_alloc(innesco->size1,N_h);
+  gsl_matrix_set_zero(O_sim);
+  
+  //LMM
+  //Inizializzazione dei buffer
+  const unsigned p=innesco->size2-1;
+  gsl_matrix* Buffer_O=gsl_matrix_alloc(innesco->size1,p+1);
+  gsl_matrix* Buffer_F=gsl_matrix_alloc(innesco->size1,p+1);
+  gsl_vector* CombA=gsl_vector_alloc(innesco->size1);
+  gsl_vector* CombB=gsl_vector_alloc(innesco->size1);
+  gsl_vector* f_1=gsl_vector_alloc(innesco->size1);
+  gsl_vector* PF_tmp=gsl_vector_alloc(innesco->size1);
+  
+  for(unsigned k=0; k<p+1; ++k){
+    gsl_vector_view col_k_O=gsl_matrix_column(innesco,k);
+    gsl_vector_view col_p_k_F=gsl_matrix_column(Buffer_F,p-k);
+    gsl_matrix_set_col(O_sim,k,&(col_k_O.vector));
+    
+    double t_k=(double)k*h;
+    f_ODE(t_k,&(col_k_O.vector),&(col_p_k_F.vector));
+    gsl_matrix_set_col(Buffer_O,p-k,&(col_k_O.vector));
+  }
+  
+  double t=(double)(p+1)*h;
+  for(unsigned k=p+1;k < N_h; ++k){
+    gsl_vector_view O_k=gsl_matrix_column(O_sim,k);
+    
+    gsl_blas_dgemv(CblasNoTrans,1.0,Buffer_O,A,0.0,CombA);
+    gsl_blas_dgemv(CblasNoTrans,1.0,Buffer_F,B,0.0,CombB);
+    gsl_vector_scale(CombB,h);
+    
+    //Se esplicito
+    if(b_1 == 0.0){
+      gsl_vector_add(CombA,CombB);
+      gsl_vector_add(&(O_k.vector),CombA);
+    }else{ //Se implicito
+      gsl_vector_view O_k_1=gsl_matrix_column(O_sim,k-1);
+      gsl_vector_memcpy(&(O_k.vector), &(O_k_1.vector));
+    
+      //Iterazioni di punto fisso
+      double epsilonPF=1e-14,errore=GSL_POSINF;
+      unsigned MAX_ITERS=50;
+      unsigned j=1;
+      while(errore >= epsilonPF && j <= MAX_ITERS){    
+        f_ODE(t,&(O_k.vector),f_1); //Termine implicito
+        gsl_vector_scale( f_1,b_1*h );
+        gsl_vector_add(f_1,CombB);
+        gsl_vector_add(f_1,CombA);
+        gsl_vector_memcpy(PF_tmp,&(O_k.vector));
+        gsl_vector_memcpy(&(O_k.vector),f_1);
+        
+        //Uso f_1 per il calcolo dell'errore
+        gsl_vector_sub(PF_tmp,f_1);
+        errore=gsl_blas_dnrm2(PF_tmp);
+        ++j;
+      }
+    }
+    
+    //swap buffers
+    for(unsigned j=0; j<p; ++j){
+      gsl_matrix_swap_columns(Buffer_O,p-j,p-j-1);
+      gsl_matrix_swap_columns(Buffer_F,p-j,p-j-1);
+    }
+    f_ODE(t,&(O_k.vector),f_1);
+    gsl_matrix_set_col(Buffer_O,0,&(O_k.vector));
+    gsl_matrix_set_col(Buffer_F,0,f_1);
+  }
+  gsl_matrix_free(Buffer_O);
+  gsl_matrix_free(Buffer_F);
+  gsl_vector_free(CombB);
+  gsl_vector_free(CombA);
+  gsl_vector_free(PF_tmp);
+  gsl_vector_free(f_1);
+  return O_sim;
+}
