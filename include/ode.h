b29010ae8dfe9f592d5d75fef77c3002151c3234
Torrki <kioer.exe@gmail.com>
2025-07-25 13:31:41 +0200

    Modificate le funzioni per ottenere anche l'ultimo indice della condizione di uscita
    Signed-off-by: Torrki <kioer.exe@gmail.com>

--- include/ode.h
+++ include/ode.h
@@ -2,9 +2,9 @@
 #include <gsl/gsl_matrix.h>
 #include <stdbool.h>
 
-gsl_matrix* EuleroAvanti(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione);
-gsl_matrix* EuleroIndietro(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione);
-gsl_matrix* CrankNicolson(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione);
-gsl_matrix* Heun(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione);
-gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0, gsl_matrix* A, gsl_vector* B,bool (*condizione)(double,gsl_vector*), double *tCondizione);
-gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_matrix* innesco, gsl_vector* A, gsl_vector* B, double b_1,bool (*condizione)(double,gsl_vector*), double *tCondizione);
+gsl_matrix* EuleroAvanti(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione);
+gsl_matrix* EuleroIndietro(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione);
+gsl_matrix* CrankNicolson(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione);
+gsl_matrix* Heun(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione);
+gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0, gsl_matrix* A, gsl_vector* B,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione);
+gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_matrix* innesco, gsl_vector* A, gsl_vector* B, double b_1,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione);
--- src/ode.c
+++ src/ode.c
@@ -1,8 +1,9 @@
 #include <math.h>
 #include <gsl/gsl_blas.h>
 #include "../include/ode.h"
+#include <stdio.h>
 
-gsl_matrix* EuleroAvanti(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T,double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione){
+gsl_matrix* EuleroAvanti(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T,double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*), double *tCondizione,unsigned* indiceCondizione){
   const unsigned N_h=(unsigned)(floor(T/h)+1.0);
   
   //Allocazione matrice
@@ -31,10 +32,11 @@ gsl_matrix* EuleroAvanti(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0
   }
   gsl_vector_free(dy_Buffer);
   *tCondizione = k==N_h ? t0+T : t-h; //Istante finale della simulazione
+  *indiceCondizione=k-1;
   return O_sim;
 }
 
-gsl_matrix* EuleroIndietro(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*),double *tCondizione){
+gsl_matrix* EuleroIndietro(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*),double *tCondizione,unsigned* indiceCondizione){
   const unsigned N_h=(unsigned)(floor(T/h)+1.0);
   
   //Allocazione matrice
@@ -78,10 +80,11 @@ gsl_matrix* EuleroIndietro(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double 
   gsl_vector_free(dy_Buffer);
   gsl_vector_free(PF_tmp);
   *tCondizione = k==N_h ? t0+T : t-h; //Istante finale della simulazione
+  *indiceCondizione=k-1;
   return O_sim;
 }
 
-gsl_matrix* CrankNicolson(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*),double *tCondizione){
+gsl_matrix* CrankNicolson(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*),double *tCondizione,unsigned* indiceCondizione){
   const unsigned N_h=(unsigned)(floor(T/h)+1.0);
   
   //Allocazione matrice
@@ -131,10 +134,11 @@ gsl_matrix* CrankNicolson(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t
   gsl_matrix_free(dy_Buffer);
   gsl_vector_free(PF_tmp);
   *tCondizione = k==N_h ? t0+T : t-h; //Istante finale della simulazione
+  *indiceCondizione=k-1;
   return O_sim;
 }
 
-gsl_matrix* Heun(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T,double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*),double *tCondizione){
+gsl_matrix* Heun(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T,double h, gsl_vector* y0,bool (*condizione)(double,gsl_vector*),double *tCondizione,unsigned* indiceCondizione){
   const unsigned N_h=(unsigned)(floor(T/h)+1.0);
   
   //Allocazione matrice
@@ -176,10 +180,11 @@ gsl_matrix* Heun(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double 
   }
   gsl_matrix_free(dy_Buffer);
   *tCondizione = k==N_h ? t0+T : t-h; //Istante finale della simulazione
+  *indiceCondizione=k-1;
   return O_sim;
 }
 
-gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0, gsl_matrix* A, gsl_vector* B,bool (*condizione)(double,gsl_vector*),double *tCondizione){
+gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_vector* y0, gsl_matrix* A, gsl_vector* B,bool (*condizione)(double,gsl_vector*),double *tCondizione,unsigned* indiceCondizione){
   const unsigned N_h=(unsigned)(floor(T/h)+1.0);
   
   //Allocazione matrice
@@ -236,10 +241,11 @@ gsl_matrix* RungeKuttaEsplicito(void (*f_ODE)(double,gsl_vector*,gsl_vector*),do
   gsl_vector_free(C);
   gsl_vector_free(f_k);
   *tCondizione = k==N_h ? t0+T : t-h; //Istante finale della simulazione
+  *indiceCondizione=k-1;
   return O_sim;
 }
 
-gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_matrix* innesco, gsl_vector* A, gsl_vector* B, double b_1,bool (*condizione)(double,gsl_vector*),double *tCondizione){
+gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T, double h, gsl_matrix* innesco, gsl_vector* A, gsl_vector* B, double b_1,bool (*condizione)(double,gsl_vector*),double *tCondizione,unsigned* indiceCondizione){
   const unsigned N_h=(unsigned)(floor(T/h)+1.0);
   
   //Allocazione matrice
@@ -323,5 +329,6 @@ gsl_matrix* LMM(void (*f_ODE)(double,gsl_vector*,gsl_vector*),double t0,double T
   gsl_vector_free(PF_tmp);
   gsl_vector_free(f_1);
   *tCondizione = k==N_h ? t0+T : t-h; //Istante finale della simulazione
+  *indiceCondizione=k-1;
   return O_sim;
 }
